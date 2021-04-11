99b75ec71849f2af20c1c9abba1c8ac87b7a42ce
aaronxsalo <60741625+aaronxsalo@users.noreply.github.com>
2021-04-11 13:16:00 -0400

    Add project files.

---
+++ frmLogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS376_Project
+{
+    public partial class frmLogin : Form
+    {
+        public bool verifyLogin(string un, string pw) //function will verify the username and password have been typed correctly
+        {
+            //can be expanded on later!!!
+            string corrUn = "username"; //correct username
+            string corrPw = "password"; //correct password
+            /////////////////////////////
+            
+            if (un == corrUn && pw == corrPw) //checks if arguments are correct strings
+            {
+                return true; //if they are
+            }
+            else
+            {
+                return false; //if they are not
+            }
+        }
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text; 
+            string password = txtPassword.Text;
+
+            if (verifyLogin(username, password)) //checks that login info is correct
+            {
+                this.Hide(); //removes login form
+                frmMain newForm = new frmMain(); //sets up main form
+                newForm.ShowDialog(); //shows main form
+            }
+            else //if incorrect, shows error message box
+            {
+                MessageBox.Show("Incorrect username or password entered.", "Login Error");
+            }
+        }
+    }
+}
---
+++ frmMain.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace CIS376_Project
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lstFind = new System.Windows.Forms.ListBox();
+            this.lstSaved = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnAnalytics = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnAnalytics);
+            this.groupBox1.Controls.Add(this.lstSaved);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(534, 800);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Stocks";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstFind);
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.txtSearch);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(554, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(488, 429);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Find Stocks";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(58, 27);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(424, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(10, 394);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save Stock";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // lstFind
+            // 
+            this.lstFind.FormattingEnabled = true;
+            this.lstFind.Items.AddRange(new object[] {
+            "This will be populated from the data.",
+            "It can be changed based on the search above.",
+            "Clicking a stock from the list and then clicking the button bellow saves it."});
+            this.lstFind.Location = new System.Drawing.Point(10, 53);
+            this.lstFind.Name = "lstFind";
+            this.lstFind.Size = new System.Drawing.Size(472, 329);
+            this.lstFind.TabIndex = 4;
+            // 
+            // lstSaved
+            // 
+            this.lstSaved.FormattingEnabled = true;
+            this.lstSaved.Items.AddRange(new object[] {
+            "This will be populated from the user\'s saved stocks.",
+            "Clicking a stock from the list and then clicking \"Analytics\" button will generate" +
+                " analytics.",
+            "Clicking a stock from the list and then clicking \"Delete\" button will delete save" +
+                "d stock."});
+            this.lstSaved.Location = new System.Drawing.Point(6, 19);
+            this.lstSaved.Name = "lstSaved";
+            this.lstSaved.Size = new System.Drawing.Size(522, 407);
+            this.lstSaved.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(554, 448);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(488, 364);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Settings";
+            // 
+            // btnAnalytics
+            // 
+            this.btnAnalytics.Location = new System.Drawing.Point(7, 436);
+            this.btnAnalytics.Name = "btnAnalytics";
+            this.btnAnalytics.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalytics.TabIndex = 1;
+            this.btnAnalytics.Text = "Analytics";
+            this.btnAnalytics.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(88, 436);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(7, 466);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(521, 328);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Analytics";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 847);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "Stock Browser";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox lstFind;
+        private System.Windows.Forms.Button btnAnalytics;
+        private System.Windows.Forms.ListBox lstSaved;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
